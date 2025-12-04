07e87e6cb7d966ae6f38f283612f44e11aef1ab7
Gustavo Meneguelli <gustavomeneguelli@outlook.com>
2025-12-04 10:29:47 -0300

    feat: implement auditable entities, soft delete and response DTOs
    - Extract 'Entity' base class to standardize audit properties (Id, CreatedAt, UpdatedAt, IsDeleted) across domain entities
    - Override SaveChangesAsync in AppDbContext to automatically handle audit dates and implement Soft Delete logic
    - Configure Global Query Filters to automatically exclude soft-deleted records from queries
    - Introduce ProductResponseDto and update AutoMapper to prevent over-fetching and decouple API contract from database schema

--- src/Domain/Entities/User.cs
+++ src/Domain/Entities/User.cs
@@ -1,10 +1,10 @@
+using Domain.Common;
 using Domain.Enums;
 
 namespace Domain.Entities;
 
-public class User
+public class User : Entity
 {
-    public int Id { get; set; }
     public string Username { get; set; } = string.Empty;
     public string PasswordHash { get; set; } = string.Empty;
     public UserRole Role { get; set; } = UserRole.Common;
--- src/Infrastructure/Data/AppDbContext.cs
+++ src/Infrastructure/Data/AppDbContext.cs
@@ -7,4 +7,45 @@ public class AppDbContext(DbContextOptions options) : DbContext(options)
 {
     public DbSet<Product> Products { get; set; }
     public DbSet<User> Users { get; set; }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Product>() //Exibe apenas produtos que não foram deletados
+            .HasQueryFilter(p => !p.IsDeleted);
+
+        modelBuilder.Entity<User>()
+            .HasQueryFilter(u => !u.IsDeleted);//Exibe apenas usuários que não foram deletados
+    }
+    
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        var entries = ChangeTracker.Entries<Domain.Common.Entity>();
+
+        //Para cada entidade trackeada será feita uma modificação no seu estado e propriedades
+        foreach (var entry in entries)
+        {
+            if (entry.State == EntityState.Added)
+            {
+                entry.Entity.CreatedAt = DateTime.UtcNow;
+                entry.Entity.IsDeleted = false;
+            }
+
+            if (entry.State == EntityState.Modified)
+            {
+                entry.Entity.UpdatedAt = DateTime.UtcNow;
+            }
+
+            if (entry.State == EntityState.Deleted)
+            {
+                entry.State = EntityState.Modified; 
+            
+                entry.Entity.IsDeleted = true;
+                entry.Entity.UpdatedAt = DateTime.UtcNow;
+            }
+        }
+
+        return base.SaveChangesAsync(cancellationToken);
+    }
 }
